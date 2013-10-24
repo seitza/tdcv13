a98cd362c5dcec84b5bbda9d5f40df4a79942ffe
LindaLeidig <Linda.Leidig@campus.lmu.de>
2013-10-25 01:38:16 +0200

    changes for exercise 1

--- ex1/exec.m
+++ ex1/exec.m
@@ -1,2 +1,38 @@
 %this file is supposed to be the "main" executing file, to present our
 %solutions during the exercises
+
+% close all figure windows
+close all;
+
+% clear all workspace variables
+clear;     
+
+% clear command window
+clc;       
+
+
+%% Exercise 1
+
+I = imread('lena.gif');
+kernel = [1/9, 1/9, 1/9; 1/9, 1/9, 1/9; 1/9, 1/9, 1/9];
+J_border = convolution(I, kernel, 'border');
+J_mirror = convolution(I, kernel, 'mirror');
+
+% show original image
+figure
+imagesc(I), axis equal tight off, colormap gray
+title('original image')
+
+% show convoluted image using the second border handling (border)
+figure
+imagesc(J_border), axis equal tight off, colormap gray
+title('border')
+
+% show convoluted image using the second border handling (border)
+figure
+imagesc(J_mirror), axis equal tight off, colormap gray
+title('mirror')
+
+%% Exercise 2
+
+%% Exercise 3
