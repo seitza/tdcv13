232a94883b1effee63d99a08faa97b950f973308
Dennis Mack <dennis.mack@tum.de>
2013-10-28 01:23:13 +0100

    improves speed of convolution; 4 loops are indeed faster than matrix indexing...

--- ex1/conv_dennis.m
+++ ex1/conv_dennis.m
@@ -1,33 +1,40 @@
 function J = conv_dennis(I,H,border_treatment)
-% if we do not specify border_treatment, then use 'replicate' by default
-if nargin ~= 3
-    border_treatment = 0;
-end
-    
-% first, rotate kernel and then get dimensions of kernel
-H_conv = rot90(H,2);
-
-kernel_size = size(H_conv);
-sum_weights_kernel = sum(sum(H_conv));
-if sum_weights_kernel == 0
-    sum_weights_kernel = 1;
-end
-
-half_kernel_size = floor(kernel_size/2);
-
-% now pad image accordingly
-I_padded = padarray(I,half_kernel_size,border_treatment);
-
-% get size of input image
-[a,b] = size(I);
-% create empty output image
-J = zeros([a,b]);
-
-% now loop over every element and do the convolution
-for i=1:a
-    for j=1:b
-        % add offset to work in padded image
-        J(i,j) = sum(sum(H_conv.*I_padded(i:(i+kernel_size(1)-1), j:(j+kernel_size(2)-1))));  
+    % if we do not specify border_treatment, then use 'replicate' by default
+    if nargin ~= 3
+        border_treatment = 0;
+    else
+        switch border_treatment
+            case 'mirror'
+                border_treatment = 'symmetric';
+            otherwise
+                border_treatment = 'replicate';
+        end
     end
-end
+    
+    % first, rotate kernel and then get dimensions of kernel
+    H_conv = H';
+    kernel_size = size(H_conv);
+    half_kernel_size = (kernel_size-1)/2;
+
+    % now pad image accordingly
+    I_padded = padarray(I,half_kernel_size,border_treatment);
+
+    % get size of input image
+    [a,b] = size(I);
+    % create empty output image
+    J = zeros([a,b]);
+
+    % now loop over every element and do the convolution
+    for i=1:a
+        for j=1:b
+            % add offset to work in padded image
+            summed_value = 0;
+            for r=1:kernel_size(1)
+                for s=1:kernel_size(2)
+                    summed_value = summed_value + I_padded(r+i-1,s+j-1) * H_conv(r,s);
+                end
+            end
+            J(i,j) = summed_value;
+        end
+    end
 end
