05b63a395a12e63b524a8f3466f4e13fa1c6fb7e
LindaLeidig <Linda.Leidig@campus.lmu.de>
2013-10-30 23:59:49 +0100

    final version

--- ex1/ex3_linda.m
+++ ex1/ex3_linda.m
@@ -17,14 +17,41 @@ J_Dy = convolution_linda(I, Dy, 'mirror');
 gradient_magnitude = sqrt(J_Dx.^2+J_Dy.^2);
 
 %gradient orientations
-gradient_orientations = atan2(J_Dy./J_Dx);
+gradient_orientations = atan2(J_Dy,J_Dx);
 
 %% c)
 
+[Gx, Gy] = gauss_1D_linda(1.0);
+
+Jx_smooth = convolution_linda(I, convolution_linda(Gx, Dx, 'mirror'), 'mirror');
+Jy_smooth = convolution_linda(I, convolution_linda(Gy, Dy, 'mirror'), 'mirror');
+
+%gradient magnitudes
+gradient_magnitude_smooth = sqrt(Jx_smooth.^2+Jy_smooth.^2);
+
+%gradient orientations
+gradient_orientations_smooth = atan2(Jy_smooth,Jx_smooth);
 
 %% display images
 
-% show convoluted image using the second border handling (border)
+% show gradient magnitude
 figure
-imagesc(J_mirror), axis equal tight off, colormap gray
-title('mirror')
+imagesc(gradient_magnitude), axis equal tight off, colormap gray
+title('gradient magnitude')
+
+% show gradient orientations
+figure
+imagesc(gradient_orientations), axis equal tight off, colormap gray
+title('gradient orientations')
+
+
+% show gradient magnitude smoothed
+figure
+imagesc(gradient_magnitude_smooth), axis equal tight off, colormap gray
+title('gradient magnitude smoothed')
+
+% show gradient orientations
+figure
+imagesc(gradient_orientations_smooth), axis equal tight off, colormap gray
+title('gradient orientations smoothed')
+
