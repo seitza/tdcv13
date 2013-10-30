fbd8b2c7f9d69dcd09b14b0e6a6e285b095c4f65
Dennis Mack <dennis.mack@tum.de>
2013-10-30 13:55:47 +0100

    uses noisy image in solution3 and imshow instead of imagesc

--- ex1/exec_solution_3.m
+++ ex1/exec_solution_3.m
@@ -1,19 +1,10 @@
-%this file is supposed to be the "main" executing file, to present our
-%solutions during the exercises
-
-% close all figure windows
+%% init
 close all;
+clear all;
+clc;
 
-% clear all workspace variables
-clear;     
-
-% clear command window
-clc;       
-
-%% Exercise 3
-%% calculating gradients, magnitude and orientation
-border_treatment = 'border';
-I = double(imread('lena.gif'))/255.0;
+%% load image and create filters
+I = double(rgb2gray(imread('lena_noisy.jpg')))/255.0;
 
 Dx = [[-1,0,1];
     [-1,0,1];
@@ -23,36 +14,37 @@ Dy = [[-1,-1,-1];
     [0,0,0];
     [1,1,1]];
 
-%% 3 a) and b)
-Ix = conv_dennis(I,Dx,border_treatment);
-Iy = conv_dennis(I,Dy,border_treatment);
+% [Gy,Gx] = gen_gaussian_1D_andre(3.0)
+Gy = fspecial('gaussian',[3 3], 3.0);
+Gx = Gy;
 
-Gmag = sqrt(Ix.^2 + Iy.^2);
-Gdir = atan2(Iy,Ix);
+%% 3 a) and b) - gradient creation
+Ix = conv_dennis(I, Dx, 'border');
+Iy = conv_dennis(I, Dy, 'border');
 
-%% 3 c)
-[Gy,Gx] = gen_gaussian_1D_andre(1.0);
-Ix_smooth = conv_dennis(I,conv_dennis(Dx,Gx),border_treatment);
-Iy_smooth = conv_dennis(I,conv_dennis(Dy,Gy),border_treatment);
+Imag = sqrt(Ix.^2 + Iy.^2);
+Idir = atan2(Iy,Ix);
 
-Gmag_smooth = sqrt(Ix_smooth.^2 + Iy_smooth.^2);
-Gdir_smooth = atan2(Iy_smooth,Ix_smooth);
+%% 3 c) smoothing kernel before convoluting
+Ix_smooth = conv_dennis(I,conv_dennis(Dx, Gx), 'border');
+Iy_smooth = conv_dennis(I,conv_dennis(Dy, Gy), 'border');
 
-%% plotting data
-figure('Name','Exercise 3: Image Gradients - No smoothing', 'NumberTitle', 'Off');
-subplot(1,2,1);
-imagesc(Gmag), axis equal tight off, colormap gray
-title('Gradient magnitude');
+Imag_smooth = sqrt(Ix_smooth.^2 + Iy_smooth.^2);
+Idir_smooth = atan2(Iy_smooth, Ix_smooth);
 
-subplot(1,2,2);
-imagesc(Gdir), axis equal tight off, colormap gray
-title('Gradient orientation');
+%% plotting
+figure;
+% imagesc(Imag), axis equal tight off, colormap gray;
+imshow(Imag);
 
-figure('Name','Exercise 3: Image Gradients - Smoothed derivative', 'NumberTitle', 'Off');
-subplot(1,2,1);
-imagesc(Gmag_smooth), axis equal tight off, colormap gray
-title('Smooth gradient magnitude');
+figure;
+% imagesc(Idir), axis equal tight off, colormap gray;
+imshow(Idir);
 
-subplot(1,2,2);
-imagesc(Gdir_smooth), axis equal tight off, colormap gray
-title('Smooth gradient orientation');
+figure;
+% imagesc(Imag_smooth), axis equal tight off, colormap gray;
+imshow(Imag_smooth);
+
+figure;
+% imagesc(Idir_smooth), axis equal tight off, colormap gray;
+imshow(Imag_smooth);
