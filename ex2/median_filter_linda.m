75b2424fb7470eb38fba652c0f73bb4d97ddc387
LindaLeidig <Linda.Leidig@campus.lmu.de>
2013-10-31 13:48:05 +0100

    median filter

---
+++ ex2/median_filter_linda.m
@@ -0,0 +1,20 @@
+function [ J ] = median_filter_linda( I, size_x, size_y )
+    
+    I_padded = padarray(I, [size_y, size_x], 'replicate');
+    [I_rows, I_cols] = size(I);
+    
+    halfsize_x = (size_x-1)/2;
+    halfsize_y = (size_y-1)/2;
+    
+    J = zeros(size(I));
+    
+    for i = 1+halfsize_y:I_rows+halfsize_y-1
+        for j = 1+halfsize_x:I_cols+halfsize_x-1
+            values = I_padded(i-halfsize_y:i+halfsize_y, j-halfsize_x:j+halfsize_x);
+            values_vector = reshape(values, 1, numel(values));
+            J(i,j) = median(values_vector);
+        end
+    end
+
+end
+
