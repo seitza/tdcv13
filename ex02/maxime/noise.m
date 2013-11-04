21132a4febfd01c996c75826cdf6014957ed227e
MaximeHarmant <maxime.harmant@etu.emse.fr>
2013-11-04 14:37:45 +0100

    gaussian & sp noises

---
+++ ex02/maxime/noise.m
@@ -0,0 +1,14 @@
+function [ J ] = noise( I, type, sigma )
+
+    % Type of noise =  gaussian or salt and pepper
+    
+    if nargin == 3 && strcmp(type, 'gaussian')
+        J = I + randn(size(I)).*sigma;
+        J(J>255) = 255;
+    elseif nargin == 2 && strcmp(type, 's&p')
+        J = I + randi([0 255], size(I));
+        J(J>255) = 255;
+    end
+
+end
+
