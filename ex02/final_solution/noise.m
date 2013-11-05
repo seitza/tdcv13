0debf2f033cca82b6c0f9cc4fb3862e2c9bb27be
LindaLeidig <Linda.Leidig@campus.lmu.de>
2013-11-05 13:34:17 +0100

    changed the solution from maxime's to mine and used randi for s&p

--- ex02/final_solution/noise.m
+++ ex02/final_solution/noise.m
@@ -1,16 +1,17 @@
-function [ J ] = noise( I, type, sigma )
-
-    % Type of noise =  gaussian or salt and pepper
-    
-    if nargin == 3 && strcmp(type, 'gaussian')
-        J = I + randn(size(I)).*sigma;
-        J(J>255) = 255;
-        J(J<0) = 0;
-    elseif nargin == 2 && strcmp(type, 's&p')
-        J = I + randi([0 255], size(I));
-        J(J>255) = 255;
-        J(J<0) = 0;
+function [ J ] = noise( I, type, varargin )
+    % type of noise that can be added:
+    %   - gaussian noise --> 'gaussian', additional parameter: varianz
+    %   - salt and pepper noise --> 'salt&pepper'
+    if nargin == 2 && strcmp(type, 'salt&pepper')
+        r = randi([0 255], size(I));
+        J = double(I);
+        J(r == 255) = 255;
+        J(r == 0) = 0;
+    elseif nargin == 3 && strcmp(type, 'gaussian')
+        varianz = varargin{1};
+        J = double(I) + randn(size(I)).*varianz;
+    else
+        throw(MException('MATLAB:wronginput', 'wrong input parameters!'));
     end
-
 end
 
