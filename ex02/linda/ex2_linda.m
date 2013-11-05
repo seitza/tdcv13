844800db19c07ef0670b7b4652338758977ff667
LindaLeidig <Linda.Leidig@campus.lmu.de>
2013-11-05 12:51:53 +0100

    changed the representation of the solution

--- ex02/linda/ex2_linda.m
+++ ex02/linda/ex2_linda.m
@@ -20,31 +20,36 @@ G11 = gaussian_filter_linda(I_double, 11.0);
 
 %% show images
 
-figure('Name', 'Exercise 2 b', 'NumberTitle', 'off');
-subplot(3,3,[1 2 3]);
+figure('Name', 'Exercise bilateral filter', 'NumberTitle', 'off');
+subplot(2,2, 1);
 imagesc(I_double), axis equal tight off, colormap gray;
 title('original');
 
-subplot(3,3,4);
+subplot(2,2,2);
 imagesc(J1), axis equal tight off, colormap gray;
 title('bilateral filter sigma = 1');
 
-subplot(3,3,5);
+subplot(2,2,3);
 imagesc(J5), axis equal tight off, colormap gray;
 title('bilateral filter sigma = 5');
 
-subplot(3,3,6);
+subplot(2,2,4);
 imagesc(J11), axis equal tight off, colormap gray;
 title('bilateral filter sigma = 11');
 
-subplot(3,3,7);
+figure('Name', 'Exercise gaussian filter', 'NumberTitle', 'off');
+subplot(2,2, 1);
+imagesc(I_double), axis equal tight off, colormap gray;
+title('original');
+
+subplot(2,2,2);
 imagesc(G1), axis equal tight off, colormap gray;
 title('gaussian filter sigma = 1');
 
-subplot(3,3,8);
+subplot(2,2,3);
 imagesc(G5), axis equal tight off, colormap gray;
 title('gaussian filter sigma = 5');
 
-subplot(3,3,9);
+subplot(2,2,4);
 imagesc(G11), axis equal tight off, colormap gray;
 title('gaussian filter sigma = 11');
