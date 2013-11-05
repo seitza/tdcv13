8b57f447ed449306d8d1dc7d2c5eeb51702c3cca
LindaLeidig <Linda.Leidig@campus.lmu.de>
2013-11-05 13:37:46 +0100

    small corrections

--- ex02/final_solution/noise.m
+++ ex02/final_solution/noise.m
@@ -2,7 +2,7 @@ function [ J ] = noise( I, type, varargin )
     % type of noise that can be added:
     %   - gaussian noise --> 'gaussian', additional parameter: varianz
     %   - salt and pepper noise --> 'salt&pepper'
-    if nargin == 2 && strcmp(type, 'salt&pepper')
+    if nargin == 2 && strcmp(type, 's&p')
         r = randi([0 255], size(I));
         J = double(I);
         J(r == 255) = 255;
@@ -10,6 +10,8 @@ function [ J ] = noise( I, type, varargin )
     elseif nargin == 3 && strcmp(type, 'gaussian')
         varianz = varargin{1};
         J = double(I) + randn(size(I)).*varianz;
+        J(J < 0) = 0;
+        J(J > 255) = 255;
     else
         throw(MException('MATLAB:wronginput', 'wrong input parameters!'));
     end
