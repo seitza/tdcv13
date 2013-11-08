e00716df029babf8c01a31113a3330c842c12cbc
seitza <andre_seitz@web.de>
2013-11-08 23:03:16 +0100

    comments on harris detetctor

--- ex03/andre/harris_detector_andre.m
+++ ex03/andre/harris_detector_andre.m
@@ -1,4 +1,5 @@
 function [ R ] = harris_detector_andre( I_in, n, alpha, t , s0, k)
+%s0, k can be ignored, as it is set by default
 
 I = double(I_in);
 
@@ -7,25 +8,30 @@ if(nargin < 6)
     k = 1.2;
 end
 
+%calculate sigma from input parameters
 sigma_In = s0*k^n;
 sigma_Dn = 0.7*sigma_In;
 
 [gauss_y_D,gauss_x_D] = gen_gaussian_1D(sigma_Dn);
 
+%use gaussian convolution on input image
 I_g = conv2(conv2(I,gauss_x_D,'same'),gauss_y_D,'same');
 
 diff_x = [[-1,-1,-1];[0,0,0];[1,1,1]];
 diff_y = [[-1;-1;-1],[0;0;0],[1;1;1]];
 
+%find derivatives of convoluted input image
 I_dx = conv2(I_g,diff_x,'same');
 I_dy = conv2(I_g,diff_y,'same');
 
+%multiply derivatives
 I_x2 = I_dx.*I_dx;
 I_y2 = I_dy.*I_dy;
 I_xy = I_dx.*I_dy;
 
 [gauss_y_I,gauss_x_I] = gen_gaussian_1D(sigma_In);
 
+%convolute 
 I_g_x2 = sigma_Dn^2.*conv2(conv2(I_x2,gauss_y_I,'same'),gauss_x_I,'same');
 I_g_xy = sigma_Dn^2.*conv2(conv2(I_xy,gauss_y_I,'same'),gauss_x_I,'same');
 I_g_y2 = sigma_Dn^2.*conv2(conv2(I_y2,gauss_y_I,'same'),gauss_x_I,'same');
