61fa1815f47e49c0638a51431bd1b8cad1884231
LindaLeidig <Lilei123@aol.com>
2013-11-12 20:48:03 +0100

    solution that works better. I still do not know if it is good ;)

--- ex03/linda/ex2.m
+++ ex03/linda/ex2.m
@@ -5,26 +5,36 @@ close all;
 clear;
 
 
-%I = imread('lena.gif');
-%I_double = double(I);
-I = imread('harris.jpg');
-I_double = double(I(:,:,1));
+I = imread('lena.gif');
+I_double = double(I);
+%I = imread('harris.jpg');
+%I_double = double(I(:,:,1));
 
 res_level = 5;
 s0 = 1.5;
 k = 1.2;
-alpha = 0.06;
-th = 1500;
-tl = 10;
+alpha = 0.04;
+th = 1000000;
+tl = 50;
 
 points = harris_laplace( I_double, res_level, s0, k, alpha, th, tl );
 
 %% visualize
-for i = 1:res_level
-    figure('Name', ['resolution level ' num2str(i)], 'NumberTitle', 'off');
-    imagesc(I), colormap gray, axis equal tight off;
-    hold on;
-    p = points{1,i};
-    plot(p(:,2), p(:,1), 'r+');
-    title(['resolution level ' num2str(i)]);
+
+figure;
+imagesc(I);
+axis equal tight off, colormap(gray);
+hold on;
+
+for i=1:size(points,1)
+   plot(points(i,2),points(i,1),'ro','MarkerSize',(points(i,3)^2)*5); 
 end
+
+% for i = 1:res_level
+%     figure('Name', ['resolution level ' num2str(i)], 'NumberTitle', 'off');
+%     imagesc(I), colormap gray, axis equal tight off;
+%     hold on;
+%     p = points{1,i};
+%     plot(p(:,2), p(:,1), 'r+');
+%     title(['resolution level ' num2str(i)]);
+% end
--- ex03/linda/harris_laplace.m
+++ ex03/linda/harris_laplace.m
@@ -8,34 +8,34 @@ function [ harris_laplace ] = harris_laplace( I, res_level, s0, k, alpha, th, tl
 
     Dy = [-1,-1,-1;0,0,0;1,1,1];
     Dx = Dy';
+    
+%     diff_x = [[-1,-1,-1];[0,0,0];[1,1,1]];
+%     diff_y = [[-1;-1;-1],[0;0;0],[1;1;1]];
 
-    harris = cell(1, res_level);
-    laplace = zeros(size(I, 1), size(I,2), res_level+2);
+    harris = cell(1, res_level+1);
+    laplace = zeros(size(I, 1), size(I,2), res_level+3);
     laplace(:,:,1) = zeros(size(I));                    % zeros for the smallest and highest scale level
-    laplace(:,:,res_level+2) = zeros(size(I));
+    laplace(:,:,res_level+3) = zeros(size(I));
+    scales = zeros(1, res_level+1);
     % conduct harris detector for the resolution levels and store the
     % results in harris
-    for n = 1:res_level
-        harris{1,n} = harris_detector(I, n, s0, k, alpha, th);
+    for n = 1:res_level+1
+        harris{1,n} = harris_detector(I, n-1, s0, k, alpha, th);
         
         %compute laplacian
-        sigmaI = s0*(k^n);
+        sigmaI = s0*(k^(n-1));
+        scales(1, n) = k^(n-1);
         sigmaI_odd = floor(sigmaI);
         if mod(sigmaI_odd, 2) == 0
             sigmaI_odd = sigmaI_odd + 1;
         end
-        
-        gauss_mask_I = fspecial('gaussian', [3*sigmaI_odd, 3*sigmaI_odd], sigmaI_odd);
-        I_gauss = conv2(I, gauss_mask_I, 'same');
-        
-        I_g_xx = conv2(conv2(I_gauss, Dx, 'same'), Dx, 'same');
-        I_g_yy = conv2(conv2(I_gauss, Dy, 'same'), Dy, 'same');
-        
-        for i = 1:size(I,1)
-            for j = 1:size(I,2)
-                laplace(i,j,n+1) = abs(sigmaI^2*(I_g_xx(i,j)+I_g_yy(i,j)));
-            end
-        end
+
+        gauss_y_I = fspecial('gaussian', [3*sigmaI_odd, 1] ,sigmaI);
+        gauss_x_I = fspecial('gaussian', [1, 3*sigmaI_odd] ,sigmaI);
+
+        %get second order derivatives in x and y direction for all scales
+        G = conv2(conv2(I,gauss_x_I,'same'),gauss_y_I,'same');
+        laplace(:,:,n+1) = (sigmaI^2)*(abs(conv2(conv2(G,Dy,'same'),Dy,'same')+conv2(conv2(G,Dx,'same'),Dx,'same')));        
         
     end
     
@@ -43,19 +43,17 @@ function [ harris_laplace ] = harris_laplace( I, res_level, s0, k, alpha, th, tl
     laplace(laplace < tl) = 0;
     
     % check if local maxima found by harris detector are stable in laplace
-    harris_laplace = cell(1,res_level);     % stores the remaining coordinates for each scale level
+    harris_laplace = [];     % stores the remaining coordinates for each scale level
     
-    for n = 2:res_level+1
+    for n = 2:res_level+2
         h = harris{1, n-1};
-        J = [];                 % to store the remaining coordinates
         for i = 1:size(h, 1)
             row = h(i,1);
             col = h(i,2);
             if laplace(row, col, n) > laplace(row, col, n-1) && laplace(row, col, n) > laplace(row, col, n+1)
-                J = [J; row, col];
+                harris_laplace = [harris_laplace; row, col, scales(1,n-1)];
             end
         end
-        harris_laplace{1,n-1} = J;
     end
 
 end
