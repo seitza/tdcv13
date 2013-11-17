f78a761443fb5706fbb71b5b953e3010787371e4
Dennis Mack <dennis.mack@tum.de>
2013-11-17 19:12:12 +0100

    fixes bugs in normalized dlt

--- ex04/dennis/homography_test.m
+++ ex04/dennis/homography_test.m
@@ -1,1 +1,14 @@
 % This file tests the normalized_dlt function
+clear all
+clc;
+close all;
+
+ref_points = [10 13 45;
+              20 23 35;
+              1  1  1];
+          
+warped_points = [ 103 106 80;
+                  301 305 229;
+                  1 1 1];
+              
+H = normalized_dlt(ref_points, warped_points);
