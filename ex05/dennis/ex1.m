771e6e75c0dfb32645d33e0a78da331198afbb0d
Dennis Mack <dennis.mack@tum.de>
2013-11-27 22:47:03 +0100

    training phase is now completed...

--- ex05/dennis/ex1.m
+++ ex05/dennis/ex1.m
@@ -34,32 +34,52 @@ end
 clear img;
 
 %% parameters - taken from OzuysaFERNS09
-nr_robust_harris_points = 300;
+nr_robust_harris_points = 3;
 patch_size = [32; 32];
-nr_training_samples = 1000;
+
+nr_training_samples = 50;
 
 % parameters for affine transformations
 angle_range = [0; 2*pi];
 lambda_range = [0.6; 1.5];
+
+% fern parameters
+number_ferns = 2;
+depth_single_fern = 3;
 
 % iterations
 iterations = 15;
 %% find most robust harris points
 fprintf('Computing most robust harris points...\n');
 ref_img = imgs(:,:,1);
-robust_pts = find_most_robust_harris_points(ref_img, nr_robust_harris_points, ...
+robust_keypoints = find_most_robust_harris_points(ref_img, nr_robust_harris_points, ...
                                             angle_range, lambda_range, iterations);
-                                        
-% dbg
-figure();
-imagesc(ref_img), axis equal tight off, colormap gray;
-hold on;
-plot(robust_pts(:,1), robust_pts(:,2), 'gx', 'MarkerSize', 10, 'LineWidth', 2);
-hold off;
+
+% every keypoint is a single class
+nr_classes = size(robust_keypoints,1);
+
+% % dbg
+% figure();
+% imagesc(ref_img), axis equal tight off, colormap gray;
+% hold on;
+% plot(robust_keypoints(:,1), robust_keypoints(:,2), 'gx', 'MarkerSize', 10, 'LineWidth', 2);
+% hold off;
 
 %% train
 fprintf('Creating ferns...\n');
+fs = ferns(number_ferns, depth_single_fern, patch_size, nr_classes);
 
+for i=1:nr_training_samples
+    fprintf('Training fern with sample %d...\n', i);
+    % create patches for training
+    [train_patches, train_class_labels] = create_training_patches(ref_img, robust_keypoints, ...
+        patch_size, angle_range, lambda_range); 
+    % use them to train the ferns
+    fs.train(train_patches, train_class_labels);
+end
+
+% do not forget to normalize the ferns
+fs.normalize();
 
 %% classify
 
