852359988863a93788d1d4ac41541d51ceea6c0f
LindaLeidig <Lilei123@aol.com>
2013-12-12 21:55:49 +0100

    some debugging extensions

--- ex07/linda/ex2.m
+++ ex07/linda/ex2.m
@@ -20,8 +20,18 @@ for t = 1:1 %44
     p0 = f0(1:2, matches(1,:));
     pt = ft(1:2, matches(2,:));
     % compute homography of inliers
-    [H, inliers] = ransac(p0', pt', 4, 1000, 0.5, 50);
-    
+    [H, inliers] = ransac(p0', pt', 4, 500, 2.5, 75);
+    gte = vision.GeometricTransformEstimator;
+    [tform, inlierIdx] = step(gte, p0', pt');
+    figure; imagesc([I0,It]), colormap gray, axis equal tight off;
+    hold on;
+    [in, col]= find(inlierIdx);
+    for i = 1:size(in)
+        line([p0(1,in(i)), pt(1, in(i))+size(I0,2)], [p0(2,in(i)), pt(2, in(i))]);
+    end
+    figure; showMatchedFeatures(I0,It,p0(:,inlierIdx)',pt(:,inlierIdx)');
+    title('Matching inliers'); legend('inliersIn', 'inliersOut');
+
     % debugging
     figure;
     imagesc([I0,It]), colormap gray, axis equal tight off;
