a80ddcec069adb4cd67f1ed80f575a17c00b422d
LindaLeidig <Lilei123@aol.com>
2013-12-16 15:48:32 +0100

    error correction

--- ex07/linda/ex1.m
+++ ex07/linda/ex1.m
@@ -1,21 +1,42 @@
 clc; clear; close all;
 
 %%
+
+% setup sift path
+if exist('vl_version') == 0
+    disp('loading sift implementation...');
+    run('/Users/Linda/Documents/MATLAB/vlfeat-0.9.17/toolbox/vl_setup.m');
+end
+
 A = [472.3 0.64, 329.0; 0, 471.0, 268.3; 0, 0, 1]; % internal calibration matrix, fixed
 
-I0 = single(rgb2gray(imread('img_sequence/0000.png')));
+I0 = single(rgb2gray(imread('img_sequence/0000.png')))./255;
 
 % compute SIFT features
 [f0, d0] = vl_sift(I0);
-% figure;
-% imagesc(I0), colormap gray;
-% hold on;
-% scatter(f0(1,:), f0(2,:), 'Xr');
+figure;
+imagesc(I0), colormap gray;
+hold on;
+scatter(f0(1,:), f0(2,:), 'Xr');
+
+ % corners of the image figure
+ boundary = [103, 75;
+             550, 75;
+             105, 385;
+             553, 385];
+         
+min_x = min(boundary(:,1));
+max_x = max(boundary(:,1));
+min_y = min(boundary(:,2));
+max_y = max(boundary(:,2));
 
 % discard features that are not within the area of the object
-f0 = f0(:, f0(1,:)>98 & f0(1,:)<560 & f0(2,:)>70 & f0(2,:)<394);
-d0 = d0(:, f0(1,:)>98 & f0(1,:)<560 & f0(2,:)>70 & f0(2,:)<394);
-% scatter(f0(1,:), f0(2,:), 'Xg');
+valid_frames = f0(1,:) > min_x & f0(1,:) <= max_x & ...
+             f0(2,:) > min_y & f0(2,:) <= max_y;
+
+f0_rect = f0(:, valid_frames);
+d0_rect = d0(:, valid_frames);
+scatter(f0(1,:), f0(2,:), 'Xg');
 
 % compute corresponding 3D points
 m = [f0(1:2, :); ones(1,size(f0, 2))];
