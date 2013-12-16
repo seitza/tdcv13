68fdd2c2a0739c6fc9b91080d47a2b035e9cbc1b
LindaLeidig <Lilei123@aol.com>
2013-12-16 15:48:51 +0100

    works now correctly

--- ex07/linda/ex2.m
+++ ex07/linda/ex2.m
@@ -3,56 +3,43 @@ clc; clear; close all;
 %% load 3D points from the first image (ex1)
 load('ex1_result');
 
+% setup sift path
+if exist('vl_version') == 0
+    disp('loading sift implementation...');
+    run('/Users/Linda/Documents/MATLAB/vlfeat-0.9.17/toolbox/vl_setup.m');
+end
+
 correspondences = cell(1, 44);
 % run over image sequence
 for t = 1:44 %44
-    It = single(rgb2gray(imread(num2str(t,'img_sequence/%04d.png'))));
+    It = single(rgb2gray(imread(num2str(t,'img_sequence/%04d.png'))))./255;
     [ft, dt] = vl_sift(It);
-    
-    % debugging
-%     figure;
-%     imagesc([I0,It]), colormap gray, axis equal tight off;
-%     hold on;
-%     scatter(f0(1,:), f0(2,:), 'Xg');
-%     scatter(ft(1,:)+size(I0,2), ft(2,:), 'Xr');
-    
-    % compute matching points
-    [matches, scores] = vl_ubcmatch(d0, dt);
-    p0 = f0(1:2, matches(1,:));
-    pt = ft(1:2, matches(2,:));
-    % compute homography of inliers
-    [H, inliers] = ransac(p0', pt', 4, 500, 2.5, 75);
-%     gte = vision.GeometricTransformEstimator;
-%     [tform, inlierIdx] = step(gte, p0', pt');
-%     figure; imagesc([I0,It]), colormap gray, axis equal tight off;
-%     hold on;
-%     [in, col]= find(inlierIdx);
-%     for i = 1:size(in)
-%         line([p0(1,in(i)), pt(1, in(i))+size(I0,2)], [p0(2,in(i)), pt(2, in(i))]);
-%     end
-%     figure; showMatchedFeatures(I0,It,p0(:,inlierIdx)',pt(:,inlierIdx)');
-%     title('Matching inliers'); legend('inliersIn', 'inliersOut');
 
-    % debugging
-%     figure;
-%     imagesc([I0,It]), colormap gray, axis equal tight off;
-%     hold on;
-%     for i = 1:50
-%         line([p0(1,i), pt(1, i)+size(I0,2)], [p0(2,i), pt(2, i)]);
-%     end
+    % compute matching points
+    [matches, scores] = vl_ubcmatch(d0_rect, dt);
+    p0 = f0_rect(1:2, matches(1,:));
+    pt = ft(1:2, matches(2,:));
     
-    % visualize inliers
+    % compute homography of inliers
+%     [H, inliers] = ransac(p0', pt', 4, 2000, 8, 75);
+
+    [tform, inliers_t0, inliers_ti] = estimateGeometricTransform(...
+        p0', ...
+        pt', 'affine', ...
+        'MaxNumTrials', 2000, ...
+        'MaxDistance', 8);
+
+    %visualize inliers
     h = figure;
     imagesc([I0,It]), colormap gray, axis equal tight off;
     hold on;
-    for i = 1:size(inliers)
-        line([p0(1,inliers(i)), pt(1, inliers(i))+size(I0,2)], [p0(2,inliers(i)), pt(2, inliers(i))]);
+    for i = 1:size(inliers_t0, 1)
+        line([inliers_t0(i,1), inliers_ti(i,1)+size(I0,2)], [inliers_t0(i,2), inliers_ti(i,2)]);
     end
     saveas(h, num2str(t, 'img_sequence/%04d_correspondences'), 'png');
-    
-    % correspondences: 1. row: x_coords t0, 2. row: y_coords t0, 3.row: x_coords t, 4. row:
-    % y_coords t
-    correspondences{1,t} = [p0(1, inliers); p0(2, inliers); pt(1, inliers); pt(2, inliers)];
+
+
+    correspondences{1,t} = [inliers_t0, inliers_ti];
 end
 
 save('ex2_results');
