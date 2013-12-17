b2f5894d9d7718da0027ef33430dc4c5968473f4
LindaLeidig <Lilei123@aol.com>
2013-12-17 21:04:59 +0100

    correction of the energy funciton

--- ex07/linda/energy_function.m
+++ ex07/linda/energy_function.m
@@ -7,16 +7,11 @@ function [ f ] = energy_function( A, rt, M, m )
     t2 = rt(5);
     t3 = rt(6);
 
-    % create R using the Euler representation
-    Ra = [cos(ra), -sin(ra), 0; sin(ra), cos(ra), 0; 0,0,1];
-    Rb = [cos(rb), 0, sin(rb); 0,1,0; -sin(rb), 0, cos(rb)];
-    Rg = [1,0,0; 0, cos(rg), -sin(rg); 0, sin(rg), cos(rg)];
-    
-    R = Ra*Rb*Rg;
+    R = rotation_matrix(ra, rb, rg);
     T = [t1;t2;t3];
     RT = [R,T];
     
-    f = sum(sqrt(sum((A*RT*M-m).^2)));
+    f = sum(sum((A*RT*M-m).^2));
     
 %     syms e(A, RT, M, m);
 %     e(A, RT, M, m) = sum(sqrt(sum((A*RT*M-m).^2)));
