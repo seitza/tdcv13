f83cd5f0ef9a007f7fd9d8317e5a4f100da19695
MaximeHarmant <maxime.harmant@etu.emse.fr>
2013-12-18 18:15:13 +0100

    Update energy.m

--- ex07/maxime/energy.m
+++ ex07/maxime/energy.m
@@ -1,6 +1,9 @@
-function [ e ] = energy( A, ra, rb, rc, t1, t2, t3, M, m )
+function [ e ] = energy( A, RT, M, m )
 %ENERGY Summary of this function goes here
 %   Detailed explanation goes here
+
+    ra = RT(1); rb = RT(2); rc = RT(3);
+    t1 = RT(4); t2 = RT(5); t3 = RT(6);   
 
     Ra =  [cos(ra) -sin(ra) 0;
             sin(ra) cos(ra) 0;
@@ -8,18 +11,21 @@ function [ e ] = energy( A, ra, rb, rc, t1, t2, t3, M, m )
         ];
     Rb =  [cos(rb) 0 -sin(rb);
             0 1 0;
-            sin(rb) 0 cor(rb);
+            sin(rb) 0 cos(rb);
         ];
     Rc =  [1 0 0 
            0 cos(rc) -sin(rc);
            0 sin(rc) cos(rc);
         ];
     
+
+    
     R = Ra*Rb*Rc;
     T = [t1;t2;t3];
-    RT = [R T];
+    RT = [R,T];
 
-    e = sum(norm(A*RT*M-m));
+    e = sum(norm(A*RT*M));
 
 end
+
 
