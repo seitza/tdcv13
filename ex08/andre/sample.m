dd54de6f651c63bcb316b2d525dfa468c43eac2a
seitza <andre_seitz@web.de>
2014-01-16 10:15:07 +0100

    final submitted solution

--- ex08/andre/sample.m
+++ ex08/andre/sample.m
@@ -1,9 +1,9 @@
-function [ samples ] = sample( image, grid )
+function [ samples ] = sample( image, grid, weight )
 
 samples = interp2(1:size(image,2),1:size(image,1),image,grid(:,1),grid(:,2),'linear',0);  
 
 samples = (samples-mean(samples))/std(samples);
-samples = samples+0.1*rand(size(samples));
+samples = samples+weight*rand(size(samples));
 
 end
 
