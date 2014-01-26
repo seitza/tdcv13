7848c32d0eee91430f3b917a97a9db07ba10835b
Dennis Mack <dennis.mack@tum.de>
2014-01-26 12:41:29 +0100

    adds color tracking - needs to be merged with grayscale tracking

--- ex10/dennis/ex1.m
+++ ex10/dennis/ex1.m
@@ -2,6 +2,7 @@ clear all;
 close all;
 clc;
 
+%% init
 img = double(imread('lena.jpg'))/255.0;
 img_gray = rgb2gray(img);
 
@@ -16,13 +17,16 @@ w = roi(3);
 h = roi(4);
 
 template = img_gray(y:y+h-1, x:x+w-1);
+template_color = img(y:y+h-1, x:x+w-1,:);
+
 w_half = floor(w/2);
 h_half = floor(h/2);
 
 xc = x + w_half;
 yc = y + h_half;
 
-% try tracking
+%% try tracking
+tic;
 figure('Name', 'Tracking with SSD', 'NumberTitle', 'Off');
 imagesc(img_gray), axis equal tight off, colormap gray
 hold on;
@@ -30,7 +34,9 @@ plot_region([x, y, x+w-1, y+h-1], 'g');
 plot(xc, yc, 'go');
 [P_SSD,R_SSD] = track_template(img_gray, template, 'SSD');
 plot(P_SSD(1), P_SSD(2), 'rx', 'MarkerSize', 10);
+toc;
 
+tic;
 figure('Name', 'Tracking with NCC', 'NumberTitle', 'Off');
 imagesc(img_gray), axis equal tight off, colormap gray
 hold on;
@@ -38,7 +44,28 @@ plot_region([x, y, x+w-1, y+h-1], 'g');
 plot(xc, yc, 'go');
 [P_NCC,R_NCC] = track_template(img_gray, template, 'NCC');
 plot(P_NCC(1), P_NCC(2), 'rx', 'MarkerSize', 12);
+toc;
 
+%% try color tracking
+tic;
+figure('Name', 'Tracking with SSD', 'NumberTitle', 'Off');
+imagesc(img), axis equal tight off
+hold on;
+plot_region([x, y, x+w-1, y+h-1], 'g');
+plot(xc, yc, 'go');
+[P_SSD,R_SSD] = track_template_color(img, template_color, 'SSD');
+plot(P_SSD(1), P_SSD(2), 'rx', 'MarkerSize', 10);
+toc;
+
+tic;
+figure('Name', 'Tracking with NCC', 'NumberTitle', 'Off');
+imagesc(img), axis equal tight off
+hold on;
+plot_region([x, y, x+w-1, y+h-1], 'g');
+plot(xc, yc, 'go');
+[P_NCC,R_NCC] = track_template_color(img, template_color, 'NCC');
+plot(P_NCC(1), P_NCC(2), 'rx', 'MarkerSize', 12);
+toc;
 
 
 
---
+++ ex10/dennis/track_template_color.m
@@ -0,0 +1,73 @@
+function [P,R] = track_template_color(I,T, method)
+    if nargin < 3
+        method = 'SSD';
+    end
+    
+    % determine template size
+    [m,n,~] = size(I);
+    [m_t,n_t,~] = size(T);    
+    h_half_t = floor(m_t/2);
+    w_half_t = floor(n_t/2);
+    
+    if strcmp(method, 'SSD') == 1
+        R = -ones([m n]);
+    else
+        R = zeros([m n]);
+        
+        % compute mean values of all channels
+        T_mean = mean(mean(T));
+        T = T - repmat(T_mean, m_t, n_t, 1);
+        
+        % divide by std. deviation
+        T = T ./ repmat(sqrt(sum(sum(T.^2))), m_t, n_t, 1);
+    end
+    
+    switch(method)
+        case 'NCC'
+            for y=h_half_t+1:m-h_half_t
+                for x=w_half_t+1:n-w_half_t
+                    I_norm = I(y-h_half_t:y+h_half_t, x-w_half_t:x+w_half_t,:);
+                    I_norm = I_norm - repmat(mean(mean(I_norm)), m_t, n_t, 1);
+                    I_norm = I_norm ./ repmat(sqrt(sum(sum(I_norm.^2))), m_t, n_t, 1);
+                    
+                    val = zeros(3,1);
+                    for i=1:m_t
+                        for j=1:n_t
+                            for c=1:3
+                                val(c) = val(c) + (T(i,j,c)*I_norm(i,j,c));
+                            end
+                        end
+                    end
+                    R(y,x) = sum(val)/3;
+                end
+            end
+            
+            best_score = max(max(R));
+            best_match = find(R == best_score);
+            [match_y, match_x] = ind2sub(size(R), best_match);
+            P = [match_x; match_y];
+            
+        case 'SSD'
+            for y=h_half_t+1:m-h_half_t
+                for x=w_half_t+1:n-w_half_t
+                    val = zeros(3,1);
+                    for i=1:m_t
+                        for j=1:n_t
+                            for c=1:3
+                                val(c) = val(c) + (T(i,j,c) - I(y-h_half_t+i-1, x-w_half_t+j-1,c))^2;
+                            end
+                        end
+                    end
+                    R(y,x) = sum(val)/3;
+                end
+            end
+            
+            best_score = max(max(R));
+            R = best_score - R;
+
+            best_match = find(R == best_score);
+            [match_y, match_x] = ind2sub(size(R), best_match);
+            P = [match_x; match_y];
+    end
+end
+
